3a766f9dc130ec9f5e98e264551372cabc4e3270
lvdongliang <lvdongliang01@sinochem.com>
2021-04-22 10:08:53 +0800

    123

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,19 +5,18 @@
       <color-picker v-model="color" @change="onChange"></color-picker>
     </div>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 import { VueColorpicker } from '@firehole/vue-pop-colorpicker'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
     'color-picker': VueColorpicker
   },
   data () {
